92daa3dbebce49918e91743cd598e3d7934c6c4b
Heitor Cazado <heitorcazado@gmail.com>
2024-11-02 17:53:51 -0300

    catching bad contactId error

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -8,11 +8,17 @@ const controller = {
         res.status(200).json(contacts);
     },
     getSingle : async (req, res, next) =>{
-        const contactId = ObjectId.createFromHexString(req.params.id);
-        const result = await mongodb.getDb().db("project1").collection("contacts").find({_id: contactId});
-        const contacts = await result.toArray();
-        res.setHeader("Content-type", "application/json");
-        res.status(200).json(contacts[0]);
+        try{
+            const contactId = ObjectId.createFromHexString(req.params.id);
+            const result = await mongodb.getDb().db("project1").collection("contacts").find({_id: contactId});
+            const contacts = await result.toArray();
+            res.setHeader("Content-type", "application/json");
+            res.status(200).json(contacts[0]);
+        }
+        catch (error){
+            res.status(405).json("Invalid requisition."+error.message);
+        }
+        
     }
 }
 
